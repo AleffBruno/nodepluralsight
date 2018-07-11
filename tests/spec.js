a7f2bfc308eaeffe75c19e9d5db2146c1bd93c96
Aleff <aleffbruno@gmail.com>
2018-07-11 16:47:04 -0300

    testing in progress .....  delete_test

--- tests/spec.js
+++ tests/spec.js
@@ -4,7 +4,7 @@ var chai = require('chai').expect;
 
 describe('[LIONS]',function(){
 
-    it('should get all errors',function(){
+    it('should get all errors',function(done){
         request(app)
         .get('/lions')
         .set('Accept','application/json')
@@ -15,29 +15,39 @@ describe('[LIONS]',function(){
             done();
         })
     });
-});
 
-
-describe('[LIONS post]',function(){
-    it('should post a lion /POST',function(){
+    it('should create a lion',function(done){
         request(app)
         .post('/lions')
         .send({name:'simba'})
         .set('Accept','application/json')
         .expect('Content-Type','application/json')
-        .expect(200)
+        .expect(201)
         .end(function(err,resp){
-            if(err){
-                done("ERRO AQUI"+err);
-            }
-            chai(resp.body).to.be.an('json');
+            chai(resp.body).to.be.an('object');
             done();
         })
     });
+
+    it('should delete a lion',function(done){
+        request(app)
+        .post('/lions')
+        .send({name:'mufasa'})
+        .set('Accept','application/json')
+        .end(function(err,resp){
+            var lion = resp.body;
+            request(app)
+            .delete('/lions/'+lion.id)
+            .end(function(err,resp){
+                chai(resp.body).to.eql(lion);
+                done();
+            });
+        });
+    });
+
+
 });
 
 
-
-            
 
 
